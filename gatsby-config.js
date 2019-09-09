b434002c1b03437a605aeb9b3beb7f83f0d81562
Aman <aman29271@gmail.com>
2019-09-10 01:46:25 +0530

    dynamic image loading

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,54 +3,73 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-const config = require('./data/siteConfig')
-const [github,twitter,linkedin]  = config.userLinks
+const config = require("./data/siteConfig")
+const [github, twitter, linkedin] = config.userLinks
 module.exports = {
   /* Your site config here */
-  siteMetadata:{
-    title:config.siteTitle,
-    author:config.userName,
-    Email:config.userEmail,
+  siteMetadata: {
+    title: config.siteTitle,
+    author: config.userName,
+    Email: config.userEmail,
     githubUsername: github.username,
-    twitterUsername:twitter.username,
-    linkedInUsername:linkedin.username,
+    twitterUsername: twitter.username,
+    linkedInUsername: linkedin.username,
     defaultTitle: config.siteTitle,
     defaultDescription: config.siteDescription,
     siteUrl: config.siteUrl,
     defaultImage: config.siteLogo,
   },
-  plugins:[
-    'gatsby-plugin-sass',
+  plugins: [
+    "gatsby-plugin-sass",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/`,
         name: `content`,
-      }},
-      {
+      },
+    },
+    {
+      resolve: `gatsby-source-mongodb`,
+      options: {
+        connectionString:
+          `mongodb+srv://aman:${process.env.PASSWORD}@cluster0-pe0lk.mongodb.net/test?retryWrites=true&w=majority`,
+        extraParams: {
+          replicaSet: "cluster0-shard-0",
+          ssl: true,
+          authSource: "admin",
+        },
+        dbName: "ace",
+        collection: ["People","team_2018-19"],
+        clientOptions: {
+          useNewUrlParser: true,
+        },
+      },
+    },
+    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
       },
     },
-    'gatsby-plugin-draft',
+    "gatsby-plugin-draft",
     {
-      resolve:`gatsby-transformer-remark`,
-      options:{
-        plugins:[
-          'gatsby-remark-relative-images','gatsby-remark-prismjs',"gatsby-remark-copy-linked-files",
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          "gatsby-remark-relative-images",
+          "gatsby-remark-prismjs",
+          "gatsby-remark-copy-linked-files",
           {
-            resolve:`gatsby-remark-images`,
-            options:{
+            resolve: `gatsby-remark-images`,
+            options: {
               maxWidth: 750,
-              linkImagesToOriginal: false
-            }
-          }
-        ]
-      }
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
+      },
     },
-    'gatsby-plugin-sharp',
+    "gatsby-plugin-sharp",
     `gatsby-transformer-sharp`,
-
-  ]
+  ],
 }
